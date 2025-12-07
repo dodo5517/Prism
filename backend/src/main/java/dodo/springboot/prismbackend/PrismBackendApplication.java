58e716ceeb9143c2ecc95e735f966b823ca47d00
dodo5517 <kh9015548@gmail.com>
2025-12-07 17:55:07 +0900

    Initial commit: Prism Setup

---
+++ backend/src/main/java/dodo/springboot/prismbackend/PrismBackendApplication.java
@@ -0,0 +1,13 @@
+package dodo.springboot.prismbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrismBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrismBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/dodo/springboot/prismbackend/PrismBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dodo.springboot.prismbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrismBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
